904efdf5b880da6b3c0cf607261e94f90efe377e
Junior <juniorkart@hotmail.com>
2019-04-24 18:12:26 -0300

    Criação de routas

---
+++ src/js/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import NewGame from './../components/newGame/NewGame'
+import OptionsGame from './../components/optionGame/OptionsGame'
+
+class Routes extends React.Component {
+    render() {
+        return (
+            <main className="container">
+                <Switch>
+                    <Route exact path='/' component={ OptionsGame }/>
+                    <Route path='/new-game' component={ NewGame }/>
+                </Switch>
+            </main>
+        )
+    }
+}
+export default Routes
